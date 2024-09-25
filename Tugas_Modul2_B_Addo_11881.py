17612ba0f6052677fe94832e0c24aadd1d70734e
Alouisius Gonzaga Duan Allegra <220711881@students.uajy.ac.id>
2024-09-25 15:17:19 +0700

    Update Tugas_Modul2_B_Addo_11881.py

--- Tugas_Modul2_B_Addo_11881.py
+++ Tugas_Modul2_B_Addo_11881.py
@@ -2,8 +2,7 @@ import streamlit as st
 import pickle
 import os
 
-model_path = r'C:\Users\Administrator\Documents\VSCode\Tugas_B_11881'
-model=os.path. join(model_path,'GBT_heartDisease_model.pkl')
+model='GBT_heartDisease_model.pkl'
 
 with open(model,'rb') as f:
     loaded_model = pickle.load(f)
